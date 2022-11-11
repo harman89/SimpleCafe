511f6b0cb6cec079e23222426d282e26e18ef2d2
harman89 <60431061+harman89@users.noreply.github.com>
2022-11-11 14:12:09 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/simplecafe/MainActivity.kt
+++ app/src/main/java/com/example/simplecafe/MainActivity.kt
@@ -3,25 +3,15 @@ package com.example.simplecafe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.example.simplecafe.databinding.ActivityMainBinding
+import com.example.simplecafe.ViewModel.CafeViewModel
 import com.example.simplecafe.view.AuthorizationFragment
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var  binding:ActivityMainBinding
     private val cafeViewModel : CafeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*var name:String? = ""
-        var password:String? = ""
-        var order:String? = ""
-        var additions:String? = ""
-        cafeViewModel.orderData.observe(this){
-            name = it.Name
-            password = it.Password
-            order = it.Order
-            additions = it.Additions
-        }*/
+
         if (savedInstanceState==null)
         {
             supportFragmentManager.beginTransaction().add(R.id.frameLayoutMain,
