b783781cc9f10f7b4811d28b139b49e8139f00bb
harman89 <60431061+harman89@users.noreply.github.com>
2022-11-11 14:12:49 +0500

    Update CafeViewModel.kt

--- app/src/main/java/com/example/simplecafe/CafeViewModel.kt
+++ app/src/main/java/com/example/simplecafe/CafeViewModel.kt
@@ -1,11 +1,21 @@
-package com.example.simplecafe
+package com.example.simplecafe.ViewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.simplecafe.data.model.OrderData
+import com.example.simplecafe.data.repository.OrderRepositoryClass
 
-open class CafeViewModel : ViewModel(){
-    val orderData : MutableLiveData<OrderData> by lazy{
-        MutableLiveData<OrderData>()
+open class CafeViewModel (private val repository : OrderRepositoryClass = OrderRepositoryClass()): ViewModel(){
+    private val _orders = MutableLiveData<OrderData>()
+    val orders: LiveData<OrderData>
+        get() = _orders
+
+    fun getOrder() {
+        _orders.value = repository.getOrder()
+    }
+
+    fun setOrder(order: OrderData) {
+        repository.setOrder(order)
     }
 }
